e2a315fb1a90bed21f4bd2fb434db94f5dc8dfe5
averagejoesec <root@TBT-averagejoesec>
2024-12-20 10:48:45 +0100

    add links, specify language, add subject

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import requests
 from send_email import send_email
 
 api_key = "ENV_VARIABLE" # This should be stored as an external variable
-url = f"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey={api_key}"
+url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&" \
+        f"apiKey={api_key}&" \
+        "language=en"
 
 # Make a requests
 request = requests.get(url)
@@ -12,9 +14,12 @@ content = request.json()
 
 # Access the article title and description
 body = ""
-for article in content['articles']:
+for article in content['articles'][:20]:
     if article['title'] is not None:
-        body = body + article['title'] + "\n" + article['description'] + 2*"\n"
+        body = "Subject: Today's Tech News" + "\n" \
+        + body + article['title'] + "\n" \
+        + article['description'] + "\n" \
+        + article["url"] + 2*"\n"
     
 
 body = body.encode('utf-8')
